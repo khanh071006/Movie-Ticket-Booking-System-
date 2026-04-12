b64e721115aa9df83e8a6273bea5d16049157b1d
Khanh Dong Gia <donggiakhanh123456@gmail.com>
2026-04-12 15:09:09 +0700

    feat: implement standardized ApiResponse format

---
+++ src/main/java/com/example/Movie_Ticket_Booking_System/controller/AccountController.java
@@ -0,0 +1,4 @@
+package com.example.Movie_Ticket_Booking_System.controller;
+
+public class AccountController {
+}
---
+++ src/main/java/com/example/Movie_Ticket_Booking_System/service/account/AccountService.java
@@ -0,0 +1,4 @@
+package com.example.Movie_Ticket_Booking_System.service.account;
+
+public interface AccountService {
+}
