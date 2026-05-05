33aadf554462fbaa682f0ac8437111af00f3f9dc
Khanh Dong Gia <donggiakhanh123456@gmail.com>
2026-05-05 10:42:38 +0700

    feat(role): implement full CRUD operations for roles

--- src/main/java/com/example/Movie_Ticket_Booking_System/features/role/AccountRoleRepository.java
+++ src/main/java/com/example/Movie_Ticket_Booking_System/features/role/AccountRoleRepository.java
@@ -1,8 +1,10 @@
 package com.example.Movie_Ticket_Booking_System.features.role;
 
+import com.example.Movie_Ticket_Booking_System.features.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRoleRepository extends JpaRepository<AccountRole, Integer> {
+    void deleteByAccount(Account account);
 }
